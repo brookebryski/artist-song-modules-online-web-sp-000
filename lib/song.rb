a6ee92b0471df2fbe50eb5668189c479883d59bd
chic-state-0895 <brookeabryski@gmail.com>
2020-09-18 19:29:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,10 +9,6 @@ include Memorable::InstanceMethods
 
   @@songs = []
 
-  def initialize
-    self.class.all << self
-  end
-
   def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
   end
